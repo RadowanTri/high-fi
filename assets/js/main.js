11adfeb9bbcfdedcd40bda35b3543b91727edf20
RadowanTri <radowan.bhuiyan.tri@gmail.com>
2022-02-14 12:37:04 +0600

    editor

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,110 +13,12 @@ $(document).ready(function($) {
     //     });
      // smartSpeed: 5000,
     // Banner slider
-        var $companiesSlider = $(".home-slider");
-        $companiesSlider.owlCarousel({
-            loop: true,
+        var $appartmentSlider = $(".appartment-slider");
+        $appartmentSlider.owlCarousel({
+            loop: false,
             nav: true,
             navText: [
-            '<i class="fa fa-angle-left"></i>',
-            '<i class="fa fa-angle-right"></i>',
-            ],
-            dots: true,
-            autoplayTimeout:7000,
-            smartSpeed: 5000,
-            autoplay: false,
-            margin: 30,
-            responsive: {
-            0: {
-                items: 1,
-            },
-            768: {
-                items: 1,
-            },
-            992: {
-                items: 1,
-            },
-            1399: {
-                items: 1,
-            },
-            }
-        });
-
-    // Magnific popup
-        $('.videos-icon').magnificPopup({
-            type:'iframe',
-            iframe: {
-            patterns: {
-            youtube: {
-                index: 'youtube.com/', 
-
-                id: 'v=', 
-                src: 'http://www.youtube.com/embed/%id%?autoplay=1' 
-            }
-
-            },
-            srcAction: 'iframe_src',
-            }
-        });
-           // focusSlider 
-        var $focusSlider = $(".focus-slider");
-        $focusSlider.owlCarousel({
-            loop: true,
-            nav: false,
-            dots: true,
-            autoplayTimeout:7000,
-            autoplay: true,
-            margin: 30,
-            responsive: {
-                0: {
-                    items: 1,
-                },
-                768: {
-                    items: 1,
-                },
-                992: {
-                    items: 1,
-                },
-                1399: {
-                    items: 1,
-                },
-            }
-        });
-           // collaborator-slider 
-        var $collaboratorSlider = $(".collaborator-slider");
-        $collaboratorSlider.owlCarousel({
-            loop: true,
-            nav: true,
-            navText: [
-            '<i class="fa fa-long-arrow-alt-left"></i>',
-            '<i class="fas fa-long-arrow-alt-right"></i>',
-            ],
-            dots: false,
-            autoplayTimeout:7000,
-            autoplay: true,
-            margin: 30,
-            responsive: {
-            0: {
-                items: 1,
-            },
-            768: {
-                items: 3,
-            },
-            992: {
-                items: 5,
-            },
-            1399: {
-                items: 5,
-            },
-            }
-        });
-       // testimonial-slider 
-        var $testimonialSlider = $(".testimonial-slider");
-        $testimonialSlider.owlCarousel({
-            loop: true,
-            nav: true,
-            navText: [
-            '<i class="fas fa-chevron-left"></i>',
+            '<i class="fa fa-chevron-left"></i>',
             '<i class="fas fa-chevron-right"></i>',
             ],
             dots: false,
@@ -125,19 +27,17 @@ $(document).ready(function($) {
             margin: 30,
             responsive: {
             0: {
-                items: 1,
-            },
-            768: {
-                items: 1,
-            },
-            992: {
-                items: 1,
-            },
-            1399: {
                 items: 2,
             },
-   
+            768: {
+                items: 2,
+            },
+            992: {
+                items: 2,
+            },
+            1399: {
+                items: 3,
+            },
             }
         });
-
 }(jQuery));
